69c762d50128fb4514b7682fc9f9b468fc0fe4ab
Ishan Arya <ishanarya0@gmail.com>
2020-11-02 16:08:54 +0530

    Reverse Bits

---
+++ Bit-Manipulation/ReverseBits.cpp
@@ -0,0 +1,12 @@
+//https://www.interviewbit.com/problems/reverse-bits/
+
+unsigned int Solution::reverse(unsigned int A) {
+   for more details
+    int result = 0;
+    for(int i=0; i<32; i++){
+        int bit = A & 1;
+        result = ( result << 1 ) + bit;
+        A = A >> 1;
+    }
+    return result;
+}
